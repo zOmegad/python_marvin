758e9be161910abc4b4593849b5542db057909df
zOmegad <omegad@protonmail.com>
2021-08-11 10:45:00 +0200

    py

---
+++ listes.py
@@ -0,0 +1,3 @@
+fruits = ["Pommme", "Poire", "Banane", "Abricot"]
+# la liste commence avec l'élément 0 .... +x
+print(fruits[1])
---
+++ variables.py
@@ -0,0 +1,11 @@
+def aff():
+    print("Hello") # string
+    print(6+2) # integer
+
+
+prenom = input("Comment vous appelez vous ?")
+age = input("Quel as tu ?")
+
+
+print("Tu t'appeles " + prenom)
+print("Tu as " + age + " ans")
