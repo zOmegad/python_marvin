33c6ac1ffa8d2314072b66d684b25f1f88be4f48
zOmegad <omegad@protonmail.com>
2021-08-12 10:30:29 +0200

    boucle

---
+++ boucles.py
@@ -0,0 +1,11 @@
+legumes = ["Salade", "Carotte", "Choux", "Aubergine"]
+
+'''
+for x in legumes:
+    print(x)
+'''
+counter = 1
+
+for etage in range(7):
+    print("#" * counter)
+    counter += 1
