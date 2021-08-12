fa75c0c9efafdc73a72ff0964633c62a7e66d45d
zOmegad <omegad@protonmail.com>
2021-08-12 12:05:58 +0200

    cond

--- boucles.py
+++ boucles.py
@@ -1,11 +1,25 @@
-legumes = ["Salade", "Carotte", "Choux", "Aubergine"]
-
-'''
-for x in legumes:
+pyramide = ["#", "##", "###", "####"]
+##########
+# boucle sur une variable
+##########
+for x in pyramide:
     print(x)
-'''
-counter = 1
 
-for etage in range(7):
+##########
+# boucle sur une range
+##########
+counter = 1
+valeur = int(input("Choisis un nombre d'étages: "))
+
+for x in range(valeur):
     print("#" * counter)
     counter += 1
+
+#########
+# boucle sur une condition | tant que x <= y, j'execute
+#########
+etages = 1
+
+while etages <= 7:
+    print("#"*etages)
+    etages += 1
---
+++ condition.py
@@ -0,0 +1,9 @@
+heure = 4
+if heure < 4: # si heure est inferieur a 4
+    print("vert")
+elif heure > 4: # ou alors superieur à 4
+    print("rouge")
+elif heure > 20: # ou alors superieur à 20
+    print("rose")
+else: # sinon
+    print("orange")
